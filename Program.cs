1e17a63361fd2e9da6ca71cfa03c10f7ca1c45a8
preethi pattepu <preethi4737@gmail.com>
2023-05-07 10:49:21 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 100, 500, 300, 200, 400 };
+        string json = JsonConvert.SerializeObject(numbers);
+        Console.WriteLine(json);
+        Console.ReadKey();
+    }
+}
